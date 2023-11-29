4ca449295c3aa43406a57e4bc5df2980f46cd80c
Sharofiddin sass <sharofiddinnormuminov@gmail.com>
2023-11-29 16:23:59 +0500

    light and dark

--- script.js
+++ script.js
@@ -4,21 +4,47 @@ const incrementbtn = document.querySelector(".btn2");
 const increase = document.querySelector(".btn3");
 const decrease = document.querySelector(".btn4");
 const reset = document.querySelector(".btnReset");
+const toggle = document.querySelector(".light");
+const body = document.querySelector("body");
+const sunMo0nBg = document.querySelector(".light-dark");
 
-incrementbtn.addEventListener("click", () => {
-  h1.textContent = +h1.textContent + 1;
-});
-decrementbtn.addEventListener("click", () => {
-  h1.textContent = +h1.textContent - 1;
-});
-increase.addEventListener("click", () => {
-  h1.textContent = +h1.textContent + 7;
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("fa-moon");
+  if (toggle.classList.toggle("light")) {
+    sunMo0nBg.style.background = "img/sun-blog.webp";
+    body.style.background = "#fff";
+    body.style.transition = "2s";
+  } else {
+    sunMo0nBg.style.background = "img/moon2.jpg";
+    body.style.background = "#000";
+    body.style.transition = "2s";
+  }
 });
 
-decrease.addEventListener("click", () => {
-  h1.textContent = +h1.textContent - 5;
-});
+const foo = (value) => {
+  h1.textContent = +h1.textContent + value;
+};
 
-reset.addEventListener("click", () => {
-  h1.textContent = 0;
-});
+incrementbtn.addEventListener("click", () => foo(1));
+decrementbtn.addEventListener("click", () => foo(-1));
+increase.addEventListener("click", () => foo(7));
+decrease.addEventListener("click", () => foo(-5));
+reset.addEventListener("click", () => (h1.textContent = 0));
+
+// incrementbtn.addEventListener("click", () => {
+//   h1.textContent = +h1.textContent + 1;
+// });
+// decrementbtn.addEventListener("click", () => {
+//   h1.textContent = +h1.textContent - 1;
+// });
+// increase.addEventListener("click", () => {
+//   h1.textContent = +h1.textContent + 7;
+// });
+
+// decrease.addEventListener("click", () => {
+//   h1.textContent = +h1.textContent - 5;
+// });
+
+// reset.addEventListener("click", () => {
+//   h1.textContent = 0;
+// });
